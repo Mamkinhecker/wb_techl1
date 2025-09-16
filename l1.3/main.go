06ff1a4d4dd18dc39242bb00b7da8118c223e4b4
матвей <mamkinhecker@Noutbuk-matvej.local>
2025-09-17 02:28:42 +0300

    l1.3

---
+++ l1.3/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	nWorkers := flag.Int("workers", 4, "number of workers")
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	dataChan := make(chan string)
+	var wg sync.WaitGroup
+	for i := 0; i < *nWorkers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for data := range dataChan {
+				fmt.Printf("Worker %d: %s\n", id, data)
+			}
+			fmt.Printf("Worker %d stopped\n", id)
+		}(i)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		fmt.Println("\nReceived interrupt signal, shutting down...")
+		cancel()
+		close(dataChan)
+	}()
+
+	go func() {
+		counter := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				data := fmt.Sprintf("Data %d", counter)
+				dataChan <- data
+				counter++
+				time.Sleep(time.Second)
+			}
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("All workers stopped, program terminated")
+}
