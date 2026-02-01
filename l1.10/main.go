8e149a8fd0941be315d19b73880532df6150aed1
Mamkinhecker <alsevskijmatvej@gmail.com>
2026-02-01 14:31:47 +0300

    l1.10

--- l1.10/main.go
+++ l1.10/main.go
@@ -10,7 +10,7 @@ func main() {
 	arr := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
 	sort.Float64s(arr)
 	var last float64 = 0
-	for i := range 8 {
+	for i := range arr {
 		if math.Floor(arr[i]/10) != last {
 			if i == 0 {
 				fmt.Print(math.Floor(arr[i]/10)*10, ":{", arr[i])
