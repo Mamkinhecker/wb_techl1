061fe15c9a3e1ed9b7c3bcdf88c232f6d9c74415
Mamkinhecker <alsevskijmatvej@gmail.com>
2026-02-11 10:21:43 +0300

    l1.12

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s []string
+	var n int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		var str string
+		fmt.Scan(&str)
+		s = append(s, str)
+	}
+
+	mp := make(map[string]int)
+
+	for _, word := range s {
+		mp[word]++
+	}
+
+	for word := range mp {
+		fmt.Print(word + " ")
+	}
 }
