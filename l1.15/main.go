b9b513331b7a94483a4ce119b6ae3c8755eedaa9
Mamkinhecker <alsevskijmatvej@gmail.com>
2026-03-02 10:41:57 +0300

    l1.16

--- l1.15/main.go
+++ l1.15/main.go
@@ -1,10 +1,12 @@
 package l115
 
+import "strings"
+
 var justString string
 
 func someFunc() {
-	v := createHugeString(1 << 10) // предположительно возвращает большую строку
-	justString = v[:100]           // берём первые 100 символов
+	v := createHugeString(1 << 10)      // предположительно возвращает большую строку
+	justString = strings.Clone(v[:100]) // берём первые 100 символов
 }
 
 func main() {
