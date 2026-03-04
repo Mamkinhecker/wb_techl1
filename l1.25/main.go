ace298bfe3b57f28cd1b7956709827bcb0325aff
Mamkinhecker <alsevskijmatvej@gmail.com>
2026-03-04 13:52:19 +0300

    l125

--- l1.25/main.go
+++ l1.25/main.go
@@ -1,9 +1,32 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func Sleep(d time.Duration) {
+	start := time.Now()
+	for time.Since(start) < d {
+	}
+}
+
+func Sleep2(d time.Duration) {
 	select {
 	case <-time.After(d):
 	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := range 5 {
+		wg.Add(1)
+		go func(sec int) {
+			defer wg.Done()
+			Sleep(time.Second * time.Duration(sec))
+			fmt.Printf("горутина %d подождала %d секунд\n", sec, sec)
+		}(i)
+	}
+	wg.Wait()
 }
